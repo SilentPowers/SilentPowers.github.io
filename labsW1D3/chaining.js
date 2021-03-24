e802d8ed00ca111af53ae44ebd0def092db8c1ca
SilentPowers <cnlakour@gmail.com>
2021-03-24 14:54:28 -0500

    labs

--- labsW1D3/chaining.js
+++ labsW1D3/chaining.js
@@ -1,17 +1,20 @@
 "use strict";
 
+//solution is to return the object for every method in the object
 let ladder = {
   step: 0,
   up() {
     this.step++;
+    return this;
   },
   down() {
     this.step--;
+    return this;
   },
   showStep: function() { // shows the current step
     console.log(this.step);
+    return this;
   }
 };
 
-ladder.up();
-ladder.showStep();
+ladder.up().up().showStep();
